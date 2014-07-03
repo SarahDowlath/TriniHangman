536be5a472402687b0d6b6263a661d42529b347f
SarahDowlath <sarah.dowlath@outlook.com>
2014-07-02 22:01:02 -0400

    Trini-Hangman new program

---
+++ workspace3/Trini-Hangman/src/com/example/trini_hangman/About.java
@@ -0,0 +1,19 @@
+package com.example.trini_hangman;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class About extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+	}
+	
+	
+	
+
+}
--- workspace3/Trini-Hangman/src/com/example/trini_hangman/MainActivity.java
+++ workspace3/Trini-Hangman/src/com/example/trini_hangman/MainActivity.java
@@ -1,19 +1,102 @@
 package com.example.trini_hangman;
 
+import com.example.trini_hangman.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
-	@Override
+	
+	
+	
+	private static final String TAG = "Hangman";
+	
+	
+	
+	/** Called when the activity is first created. */
+	
+	
+	public void onCreate(Bundle savedInstanceState) {  
+		 
+		 super.onCreate(savedInstanceState);   // set content view to main layout 
+		 
+		 setContentView(R.layout.activity_main);   
+		 
+		 // set OnClick Listeners for each button in the view  
+		 Button continueButton = (Button) this.findViewById(R.id.continueBtn);   
+		 
+		 // continue button  
+		 continueButton.setOnClickListener(this);   
+		 
+		 Button newgameButton = (Button) this.findViewById(R.id.newgameBtn);   
+		 // new game button   
+		 newgameButton.setOnClickListener(this);   
+		 
+		 Button aboutButton = (Button) this.findViewById(R.id.aboutBtn);   
+		 // about button   
+		 aboutButton.setOnClickListener(this);   
+		 
+		 Button exitButton = (Button) this.findViewById(R.id.exitgameBtn);   
+		 // exit button   
+		 exitButton.setOnClickListener(this);  
+		 }  
+		 
+	
+	
+	//insert code for music Pause and Resume
+	
+	//insert code for onCreate
+	
+	//insert code for options menu
+	
+
+	 public void onClick(View v) {  
+		  switch (v.getId()) {   
+		  case R.id.continueBtn:    
+		  // continue old game    
+			//startGame(Game.CATEGORY_CONTINUE);    
+			break;   
+		  
+		  //case R.id.newgameBtn:    
+		  // open a new game dialog box    
+			//to do:
+			  //OpenNewGameDialog();    
+		//	break;
+			
+		  case R.id.aboutBtn:    
+		  // start about activity    
+			Intent i = new Intent(MainActivity.this, About.class);    
+			startActivity(i);    
+			break;  
+			
+		  case R.id.exitgameBtn:    
+		  // exit game    
+			finish();    
+			break;   
+			}  
+		}
+	
+	
+	
+	
+	
+	
+	
+	/*
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -45,8 +128,7 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 * A placeholder fragment containing a simple view.
-	 */
+	
 	public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
@@ -59,6 +141,6 @@ public class MainActivity extends Activity {
 					false);
 			return rootView;
 		}
-	}
+	}*/
 
 }
